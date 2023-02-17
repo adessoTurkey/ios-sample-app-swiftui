51c34ff75778d28a0cdd52f65fc364cf7e03fb8d
Ege Sucu <Ege.sucu@adesso.com.tr>
2023-02-17 16:26:05 +0300

    [SASU-0004] Removed Delegates and add new way.

--- SampleAppSwiftUI/Application/boilerplate-ios-swiftuiApp.swift
+++ SampleAppSwiftUI/Application/boilerplate-ios-swiftuiApp.swift
@@ -10,11 +10,44 @@ import SwiftUI
 
 @main
 struct SampleAppSwiftUIApp: App {
-    // swiftlint:disable:next weak_delegate
-    @UIApplicationDelegateAdaptor var delegate: BoilerPlateAppDelegate
+    @Environment(\.scenePhase) private var phase
+        // Check out https://developer.apple.com/documentation/swiftui/scenephase for more information
+    private var loggingService: LoggingService
+
+    init() {
+        loggingService = LoggingService()
+    }
+
     var body: some Scene {
         WindowGroup {
             MainView()
+                .onChange(of: phase, perform: manageChanges(for:))
+                .onOpenURL(perform: onOpenURL(_:))
+        }
+    }
+
+    private func manageChanges(for phase: ScenePhase) {
+        switch phase {
+            case .active:
+                // App became active
+                activated()
+            case .background:
+                // App is running in the background
+                backgrounded()
+            case .inactive:
+                // App became inactive
+                deactivated()
+            @unknown default:
+                // Fallback for future cases
+                break
         }
     }
 }
+
+// MARK: - App Life Cycle
+ extension SampleAppSwiftUIApp {
+     func activated() {}
+     func backgrounded() {}
+     func deactivated() {}
+     func onOpenURL(_ url: URL) {} // URL Opening management. Same as AppDelegate's `application(_:open:options:)`
+ }

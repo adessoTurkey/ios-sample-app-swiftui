ed9cf7e724dfca7db23f3f83c202496770e90d25
Teyhan USLU <53153396+TeyUs@users.noreply.github.com>
2023-05-30 18:17:20 +0300

    [0047] WebSocketService connect and receive methods tests

--- SampleAppSwiftUITests/Network/WebSocket/WebSocketServiceTest.swift
+++ SampleAppSwiftUITests/Network/WebSocket/WebSocketServiceTest.swift
@@ -7,13 +7,36 @@
 
 @testable import SampleAppSwiftUI
 import XCTest
+import Combine
 
 final class WebSocketServiceTest: XCTestCase {
 
-    func test_connect_returnsNilIfEndPointIsInvalid() {
+    func test_connect_returnsNil_ifEndPointIsInvalid() {
         let sut = makeSUT()
         let endPoint = TestTargetEndpoint(path: invalidUrl())
         XCTAssertNil(sut.connect(endPoint: endPoint), "Expected nil, while invalid Url, got sut \(String(describing: sut.stream))")
+    }
+
+    func test_connect_returnsItself_ifEndPointIsValid() {
+        let sut = makeSUT()
+        let endPoint = TestTargetEndpoint(path: anyWssUrl())
+//        XCTAssertNil(sut.connect(endPoint: endPoint), "Expected Itself, while valid Url, got sut \(String(describing: sut.stream))")
+        XCTAssertNotNil(sut.connect(endPoint: endPoint))
+    }
+
+    func test_receive_receiveNotCalled_IfCalledWithoutConnect() {
+        let sut = makeSUT()
+        let subscriber = SubscriberSpy()
+        sut.receive(subscriber: subscriber)
+        XCTAssertFalse(subscriber.didReceiveCalled)
+    }
+
+    func test_receive_receiveCalled_IfCalledWithConnect() {
+        let sut = makeSUT()
+        let subscriber = SubscriberSpy()
+        sut.connect(endPoint: TestTargetEndpoint(path: anyWssUrl()))?
+            .receive(subscriber: subscriber)
+        XCTAssertTrue(subscriber.didReceiveCalled)
     }
 
     // MARK: Helpers
@@ -21,11 +44,34 @@ final class WebSocketServiceTest: XCTestCase {
         "Invalid Letter: Ü"
     }
 
+    func anyWssUrl() -> String {
+        "wss://test"
+    }
+
     func makeSUT() -> WebSocketService {
-        WebSocketService.shared
+        WebSocketService()
     }
 
     struct TestTargetEndpoint: TargetEndpointProtocol {
         var path: String
+    }
+
+    class SubscriberSpy: Subscriber {
+        typealias Input = URLSessionWebSocketTask.Message
+        typealias Failure = Error
+
+        private(set) var didReceiveCalled = false
+
+        func receive(_ input: URLSessionWebSocketTask.Message) -> Subscribers.Demand {
+
+                didReceiveCalled = true
+            return Subscribers.Demand.none
+        }
+        func receive(subscription: Subscription) {
+            didReceiveCalled = true
+        }
+        func receive(completion: Subscribers.Completion<Failure>) {
+            didReceiveCalled = true
+        }
     }
 }

d29aaf7b0c068ee048eb8e43d866c880b40f1deb
Teyhan USLU <53153396+TeyUs@users.noreply.github.com>
2023-06-01 11:57:09 +0300

    [0047] added ConfigurationProtocol tests into ConfigurationTest
    ConfigurationProtocol'deki switch farklı dizayn edilmesi gerekirdi diye düşünüyorum.

--- SampleAppSwiftUITests/Configs/ConfigurationTest.swift
+++ SampleAppSwiftUITests/Configs/ConfigurationTest.swift
@@ -37,15 +37,43 @@ final class ConfigurationTest: XCTestCase {
     }
 
     func test_allCoinBaseUrl_returnsValidURL() {
-        XCTAssertEqual(Configuration.allCoinBaseUrl,
-                       "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=%d&tsym=%@&page=%d&api_key=%@")
+        XCTAssertEqual(Configuration.allCoinBaseUrl, validAllCoinBaseUrl())
     }
 
     func test_coinApiKey_returnsEmpty() {
-        XCTAssertEqual(Configuration.coinApiKey, "")
+        XCTAssertEqual(Configuration.coinApiKey, invalidString())
     }
 
     func test_webSocketBaseUrl_returnsValidURL() {
-        XCTAssertEqual(Configuration.webSocketBaseUrl, "wss://streamer.cryptocompare.com/v2?api_key=%@")
+        XCTAssertEqual(Configuration.webSocketBaseUrl, validWebSocketBaseUrl())
+    }
+
+    func test_value_throwsMissingKeyErrorWhileInvalidString() {
+        XCTAssertThrowsError(try Configuration.value(for: invalidString()) as String)
+    }
+
+    func test_value_returnsSubStringTypeWhileValidStringAsSubstring() {
+        XCTAssertTrue(type(of: (try Configuration.value(for: validString()) as Substring)) == Substring.self)
+    }
+
+    func test_value_throwsInvalidValueErrorWhileWhileValidStringAsInt() {
+        XCTAssertThrowsError(try Configuration.value(for: validString()) as Int)
+    }
+
+    // MARK: Helpers
+    func validAllCoinBaseUrl() -> String {
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=%d&tsym=%@&page=%d&api_key=%@"
+    }
+
+    func validWebSocketBaseUrl() -> String {
+        "wss://streamer.cryptocompare.com/v2?api_key=%@"
+    }
+
+    func invalidString() -> String {
+        ""
+    }
+
+    func validString() -> String {
+        "all_coin_base_url"
     }
 }

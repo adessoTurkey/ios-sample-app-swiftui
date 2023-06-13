ba13f1acc0432c72823033e7d8318d2148ce3ce6
Teyhan USLU <53153396+TeyUs@users.noreply.github.com>
2023-06-14 00:15:14 +0300

    [0047] AdessoErrorTest edited

--- SampleAppSwiftUITests/Network/Entities/AdessoErrorTest.swift
+++ SampleAppSwiftUITests/Network/Entities/AdessoErrorTest.swift
@@ -10,73 +10,110 @@ import XCTest
 
 final class AdessoErrorTest: XCTestCase {
 
+    let validURL = "https://www.adesso.com.tr/"
+    let validErrorMessage = "Error"
+    let validDomainName = "google"
+    let validNumber = 200
+    let defaultNumberIsZero = 0
+
     func test_httpErrorErrorCode_isValidNumber() {
-        guard let status = HTTPStatus(rawValue: validNumber()) else { return }
+        // GIVEN
+        guard let status = HTTPStatus(rawValue: validNumber) else { return }
+        // WHEN
         let error = AdessoError.httpError(status: status)
-        XCTAssertEqual(error.errorCode, validNumber())
+        // THEN
+        XCTAssertEqual(error.errorCode, validNumber)
     }
 
     func test_unknownErrorErrorCode_isValidNumber() {
-        let status = NSError(domain: validDomainName(), code: validNumber())
+        // GIVEN
+        let status = NSError(domain: validDomainName, code: validNumber)
+        // WHEN
         let error = AdessoError.unknown(error: status)
-        XCTAssertEqual(error.errorCode, validNumber())
+        // THEN
+        XCTAssertEqual(error.errorCode, validNumber)
     }
 
     func test_customErrorErrorCode_isValidNumber() {
-        let error = AdessoError.customError(validNumber(), validErrorMessage())
-        XCTAssertEqual(error.errorCode, validNumber())
+        // WHEN
+        let error = AdessoError.customError(validNumber, validErrorMessage)
+        // THEN
+        XCTAssertEqual(error.errorCode, validNumber)
     }
 
     func test_mappingFailedErrorCode_isZero() {
+        // WHEN
         let error = AdessoError.mappingFailed()
-        XCTAssertEqual(error.errorCode, defaultNumberIsZero())
+        // THEN
+        XCTAssertEqual(error.errorCode, defaultNumberIsZero)
     }
 
     func test_badResponseErrorCode_isZero() {
+        // WHEN
         let error = AdessoError.badResponse
-        XCTAssertEqual(error.errorCode, defaultNumberIsZero())
+        // THEN
+        XCTAssertEqual(error.errorCode, defaultNumberIsZero)
     }
 
     func test_badURLErrorCode_isZero() {
-        let error = AdessoError.badURL(validURL())
-        XCTAssertEqual(error.errorCode, defaultNumberIsZero())
+        // WHEN
+        let error = AdessoError.badURL(validURL)
+        // THEN
+        XCTAssertEqual(error.errorCode, defaultNumberIsZero)
     }
 
     func test_httpErrorResponse_isNil() {
-        guard let status = HTTPStatus(rawValue: validNumber()) else { return }
+        // GIVEN
+        guard let status = HTTPStatus(rawValue: validNumber) else { return }
+        // WHEN
         let error = AdessoError.httpError(status: status)
+        // THEN
         XCTAssertNil(error.response)
     }
 
     func test_httpErrorResponse_isValidResponse() { // TODO: Ask ErrorResponse Codable
-        guard let status = HTTPStatus(rawValue: validNumber()) else { return }
+        // GIVEN
+        guard let status = HTTPStatus(rawValue: validNumber) else { return }
+        // WHEN
         let error = AdessoError.httpError(status: status, data: dataFromErrorResponse())
+        // THEN
         XCTAssertEqual(error.response?.code, anyErrorResponse().code)
     }
 
     func test_customErrorResponse_isValidResponse() {
-        let error = AdessoError.customError(validNumber(), validErrorMessage(), dataFromErrorResponse())
+        // WHEN
+        let error = AdessoError.customError(validNumber, validErrorMessage, dataFromErrorResponse())
+        // THEN
         XCTAssertEqual(error.response?.code, anyErrorResponse().code)
     }
 
     func test_badResponseResponse_isNil() {
+        // WHEN
         let error = AdessoError.badResponse
+        // THEN
         XCTAssertNil(error.response)
     }
 
     func test_mappingFailedResponse_isNil() {
+        // WHEN
         let error = AdessoError.mappingFailed()
+        // THEN
         XCTAssertNil(error.response)
     }
 
     func test_unknownResponse_isNil() {
-        let status = NSError(domain: validDomainName(), code: validNumber())
+        // GIVEN
+        let status = NSError(domain: validDomainName, code: validNumber)
+        // WHEN
         let error = AdessoError.unknown(error: status)
+        // THEN
         XCTAssertNil(error.response)
     }
 
     func test_badURLResponse_isNil() {
-        let error = AdessoError.badURL(validURL())
+        // WHEN
+        let error = AdessoError.badURL(validURL)
+        // THEN
         XCTAssertNil(error.response)
     }
 
@@ -87,29 +124,9 @@ final class AdessoErrorTest: XCTestCase {
 
     func anyErrorResponse() -> ErrorResponse {
         let data = ErrorResponse()
-        data.code = validNumber()
-        data.message = validErrorMessage()
-        data.messages = [validErrorMessage(): validErrorMessage()]
+        data.code = validNumber
+        data.message = validErrorMessage
+        data.messages = [validErrorMessage: validErrorMessage]
         return data
-    }
-
-    func validURL() -> String {
-        "https://www.adesso.com.tr/"
-    }
-
-    func validErrorMessage() -> String {
-        "Error"
-    }
-
-    func validDomainName() -> String {
-        "google"
-    }
-
-    func validNumber() -> Int {
-        200
-    }
-
-    func defaultNumberIsZero() -> Int {
-        0
     }
 }

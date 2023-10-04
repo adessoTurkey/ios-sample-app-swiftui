41c0a4d7a147d137e1f3fe9bd2832667dacda51f
Alper Yildirim <alper.yildirim@adesso.com.tr>
2023-10-04 13:17:08 +0300

    create initial watch app

---
+++ SampleWatchAppSwiftUI Watch App/SampleWatchAppSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  SampleWatchAppSwiftUIApp.swift
+//  SampleWatchAppSwiftUI Watch App
+//
+//  Created by Yildirim, Alper on 27.09.2023.
+//
+
+import SwiftUI
+// swiftlint:disable type_name
+@main
+struct SampleWatchAppSwiftUI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+// swiftlint:enable type_name

969cf2fffad7e4b4baf7f62b1aedc8367a2c9026
Batuhan Abay <batuhan.abay@adesso.com.tr>
2023-12-19 10:11:35 +0300

    [SASU-0122] Add new NetworkService swift package to project and refactor related tuist file to add new package as targets' dependency

---
+++ Projects/NetworkService/Sources/NetworkService/NetworkService.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class AdessoNetworkService {
+    public init() {}
+
+    public func getNumber() -> Int { return 54 }
+}
